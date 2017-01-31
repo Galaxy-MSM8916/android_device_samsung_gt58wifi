351a617a6c1c0ec1f3b9deb6675aeda5d1b2ab0b
deadman96385 <seanhoyt963@gmail.com>
2017-01-31 01:24:43 -0600

    Remove omni and cm makefiles
    AndroidProducts isn't needed

--- AndroidProducts.mk
+++
@@ -1,5 +0,0 @@
-LOCAL_PATH := device/samsung/gtelwifiue
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cm.mk \
-    $(LOCAL_DIR)/lineage.mk
-#    $(LOCAL_DIR)/omni_gtelwifiue.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,3 @@
 for i in eng userdebug user; do
-	add_lunch_combo cm_gtelwifiue-${i}
 	add_lunch_combo lineage_gtelwifiue-${i}
 done
