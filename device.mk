e9e4186078206ddf122cf538a6721973e3f41682
deadman96385 <seanhoyt963@gmail.com>
2017-01-31 04:51:08 -0600

    Remove prebuilt dt.img

--- device.mk
+++ device.mk
@@ -9,9 +9,6 @@ LOCAL_PATH := device/samsung/gtelwifiue
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += device/samsung/gtelwifiue/overlay
 
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/dt.img:dt.img
-
 # Audio configuration
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/audio/acdb/Bluetooth_cal.acdb:system/etc/Bluetooth_cal.acdb \
