cd9b90f2d424f05b755ea91f13e5cc799514885f
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-27 13:33:55 -0400

    gte: Remove acdb files (move to vendor blobs)

--- device.mk
+++ device.mk
@@ -13,16 +13,6 @@ LOCAL_PATH := device/samsung/gtelwifiue
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += device/samsung/gtelwifiue/overlay
 
-# Audio configuration
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/audio/acdb/Bluetooth_cal.acdb:system/etc/Bluetooth_cal.acdb \
-	$(LOCAL_PATH)/configs/audio/acdb/General_cal.acdb:system/etc/General_cal.acdb \
-	$(LOCAL_PATH)/configs/audio/acdb/Global_cal.acdb:system/etc/Global_cal.acdb \
-	$(LOCAL_PATH)/configs/audio/acdb/Handset_cal.acdb:system/etc/Handset_cal.acdb \
-	$(LOCAL_PATH)/configs/audio/acdb/Hdmi_cal.acdb:system/etc/Hdmi_cal.acdb \
-	$(LOCAL_PATH)/configs/audio/acdb/Headset_cal.acdb:system/etc/Headset_cal.acdb \
-	$(LOCAL_PATH)/configs/audio/acdb/Speaker_cal.acdb:system/etc/Speaker_cal.acdb
-
 # Media configurations
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
